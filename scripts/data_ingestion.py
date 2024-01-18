873e757df43b2c2fadf8ae2234db6e77a40cc934
Zaid <azaidrahman@gmail.com>
2024-01-18 16:40:48 +0800

    renamed to agg_dataframes

--- scripts/data_ingestion.py
+++ scripts/data_ingestion.py
@@ -8,8 +8,7 @@ base_raw_url = 'https://raw.githubusercontent.com/MoH-Malaysia/data-darah-public
 # GitHub API URL for the contents of the data directory
 api_url = 'https://api.github.com/repos/MoH-Malaysia/data-darah-public/contents/'
 
-# DataFrames dictionary
-dataframes = {}
+agg_dataframes = {}
 
 # Send a request to the GitHub API to list the files in the repository
 response = requests.get(api_url)
@@ -27,17 +26,13 @@ if response.status_code == 200:
             # Read the CSV file directly into a DataFrame
             df = pd.read_csv(csv_url)
             
-            # Add the DataFrame to our dictionary, using the file name as the key
-            dataframes[file_info['name']] = df
+            # Add the DataFrame to dictionary, using the file name as the key
+            agg_dataframes[file_info['name'].split(".csv")[0]] = df
             
-            # Print the first few lines of the DataFrame as a check
-            print(f"First few lines of {file_info['name']}:")
-            # print(df.head())
 else:
     print("Failed to fetch repository contents")
 
 ########################################
-
 # The URL that redirects to the Parquet file
 url_granular = 'https://dub.sh/ds-data-granular'
 
@@ -50,9 +45,7 @@ if response.status_code == 200:
     file_content = response.content
     # Use BytesIO as a buffer for the binary data
     df_granular = pd.read_parquet(BytesIO(file_content), engine='pyarrow')
-    # Now df_granular contains the data from the Parquet file
-    # print(df_granular.head())
-    print(df_granular.tail())
+    
 else:
     print("Failed to download the granular data file.")
 
